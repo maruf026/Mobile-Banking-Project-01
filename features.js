68c6ea82ddd70fe0fca4aeeca1b0752be579ea3f
Maruf <adrahman117@gmail.com>
2025-02-16 16:44:29 +0600

    button fixed

--- features.js
+++ features.js
@@ -5,3 +5,7 @@ document.getElementById('addmoney-btn').addEventListener('click',function(){
 document.getElementById('cashout-btn').addEventListener('click',function(){
     showSection('cashout-sec')
 })
+
+document.getElementById('transaction-btn').addEventListener('click',function(){
+    showSection('transaction-sec')
+})
